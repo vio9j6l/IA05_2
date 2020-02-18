17aab2efbd0d59746f0c260a1074068cccee6985
JJ <jiajia.liu951ss@gmail.com>
2020-02-17 21:06:54 -0500

    5_1

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,11 @@
  Assignment: IA05
 */
 
+import java.util.Scanner;
+
 public class Main
 {
+    private static final Scanner keyboard = new Scanner(System.in);
     public static void showOff(Animal animal) {
         System.out.println(animal.getName() + ":");
         System.out.println("  Speak: '" + animal.speak() + "'");
@@ -21,19 +24,68 @@ public class Main
         Logger logger = Logger.getInstance();
         logger.log("Program started");
 
+        System.out.print("Would you like to see all animals or specific animal?");
+        System.out.println("PLease choose ");
+        System.out.println("1) Animal List");
+        System.out.println("2) One Animal");
+        System.out.print("Enter choice: ");
+        String userInput = keyboard.nextLine();
+        int choice = Integer.parseInt(userInput);
+
+        String animalChoice = null;
+        switch (choice) {
+            case 1:
+                /*
+                Animal dog = SimpleAnimalFactory.buildAnimal("dog");
+                Animal cat = SimpleAnimalFactory.buildAnimal("cat");
+                Animal bird = SimpleAnimalFactory.buildAnimal("cat");
+
+                dog.setName("Fido");
+                showOff(dog);
+
+                cat.setName("Toonces");
+                showOff(cat);
+
+                bird.setName("Tweety");
+                showOff(bird);
+                break;
+
+                 */
+                SimpleAnimalFactory.buildAnimal("dog");
+                SimpleAnimalFactory.buildAnimal("cat");
+                SimpleAnimalFactory.buildAnimal("bird");
+                break;
+
+            case 2:
+                System.out.println("PLease choose one animal");
+                System.out.println("1) Dog");
+                System.out.println("2) Cat");
+                System.out.println("2) Bird");
+                System.out.print("Enter choice: ");
+                String input = keyboard.nextLine();
+                int pet = Integer.parseInt(input);
+
+                switch (pet) {
+                    case 1:
+                        SimpleAnimalFactory.modifyAnimal("1");
+                        break;
+                    case 2:
+                        SimpleAnimalFactory.modifyAnimal("2");
+                        break;
+                    case 3:
+                        SimpleAnimalFactory.modifyAnimal("3");
+                        break;
+                }
+
+
+
+        }
+
+
         //Create a new object by calling buildAnimal() from SimpleAnimalFactory class
-        Animal dog = SimpleAnimalFactory.buildAnimal("dog");
-        Animal cat = SimpleAnimalFactory.buildAnimal("cat");
-        Animal bird = SimpleAnimalFactory.buildAnimal("bird");
 
-        dog.setName("Fido");
-        showOff(dog);
 
-        cat.setName("Toonces");
-        showOff(cat);
 
-        bird.setName("Tweety");
-        showOff(bird);
 
         //calling Logger constructor by using the getter method
         Logger logger2 = Logger.getInstance();
--- src/SimpleAnimalFactory.java
+++ src/SimpleAnimalFactory.java
@@ -10,6 +10,7 @@
 //Create new objects based on the animalType variable by if else method
 public class SimpleAnimalFactory
 {
+  /*
   public static Animal buildAnimal(String animalType) {
     Animal animalInstance = null;
     if (animalType.equals("dog")) {
@@ -18,6 +19,46 @@ public class SimpleAnimalFactory
       animalInstance = new Cat();
     } else if (animalType.equals("bird")) {
       animalInstance = new Bird();
+    }
+    return animalInstance;
+  }
+
+   */
+
+  public static Animal buildAnimal(String animalType) {
+    Animal animalInstance = null;
+    if (animalType.equals("dog")) {
+      animalInstance = new Dog();
+      animalInstance.setName("Fido");
+      Main.showOff(animalInstance);
+
+    } else if (animalType.equals("cat")) {
+      animalInstance = new Cat();
+      animalInstance.setName("Toonces");
+      Main.showOff(animalInstance);
+    } else if (animalType.equals("bird")) {
+      animalInstance = new Bird();
+      animalInstance.setName("Tweety");
+      Main.showOff(animalInstance);
+    }
+    return animalInstance;
+  }
+
+  public static Animal modifyAnimal(String animalType) {
+    Animal animalInstance = null;
+    if (animalType.equals("1")) {
+      animalInstance = new Dog();
+      animalInstance.setName("Fido");
+      Main.showOff(animalInstance);
+
+    } else if (animalType.equals("2")) {
+      animalInstance = new Cat();
+      animalInstance.setName("Toonces");
+      Main.showOff(animalInstance);
+    } else if (animalType.equals("3")) {
+      animalInstance = new Bird();
+      animalInstance.setName("Tweety");
+      Main.showOff(animalInstance);
     }
     return animalInstance;
   }
